dd17520a64e02a94c44ca06798e1946eb069277b
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-08-03 03:47:26 +0900

    イージングを書き換え

--- Framework/Math/Easing.h
+++ Framework/Math/Easing.h
@@ -7,185 +7,129 @@
 #ifndef _EASING_H_
 #define _EASING_H_
 
-//*****************************************************************************
-// 列挙体定義
-//*****************************************************************************
-enum EasingType
-{
-	None,
-	InCubic,
-	OutCubic,
-	InOutCubic,
-	Linear,
-	InExponential,
-	OutExponential,
-	InOutExponential,
-	EasingMax
-};
+#include "TMath.h"
 
 //*****************************************************************************
 // クラス宣言
 //*****************************************************************************
-template<class T>
 class Easing
 {
 public:
-	/*****************************************
-	イージング計算処理
-	引数1 float	time		:経過時間（0.0f ～ 1.0f)
-	引数2 T* start			:timeが0の時の値
-	引数3 T* goal			:timeが1の時の値
-	引数4 EasingType type	:イージングタイプ
-	******************************************/
-	static T GetEasingValue(float time, const T* start, const T* goal, EasingType type)
-	{
-		T result;
-		switch (type)
-		{
-		case EasingType::InCubic:
-			result = Easing<T>::EaseInCubic(time, start, goal);
-			break;
-
-		case EasingType::OutCubic:
-			result = Easing<T>::EaseOutCubic(time, start, goal);
-			break;
-
-		case EasingType::InOutCubic:
-			result = Easing<T>::EaseInOutCubic(time, start, goal);
-			break;
-
-		case EasingType::Linear:
-			result = Easing<T>::EaseLinear(time, start, goal);
-			break;
-
-		case EasingType::InExponential:
-			result = Easing<T>::EaseInExponential(time, start, goal);
-			break;
-
-		case EasingType::OutExponential:
-			result = Easing<T>::EaseOutExponential(time, start, goal);
-			break;
-
-		case EasingType::InOutExponential:
-			result = Easing<T>::EaseInOutExponential(time, start, goal);
-			break;
-
-		default:
-			result = *start;
-			break;
-		}
-
-		return result;
-	}
-
-private:
 	/***************************
-	InCubic計算処理
+	InCubic計算クラス
 	***************************/
-	static T EaseInCubic(float time, const T* start, const T* goal)
+	template <class T>
+	static T InCubic(float time, const T& start, const T& end)
 	{
-		if (time > 1.0f)
-			time = 1.0f;
 
-		if (time < 0.0f)
-			time = 0.0f;
+		if (time >= 1.0f)
+			return end;
 
-		return (*goal - *start) * time * time + *start;
+		if (time <= 0.0f)
+			return start;
+
+		return (end - start) * time * time + start;
+
 	}
 
 	/***************************
-	OutCubic計算処理
+	OutCubic計算クラス
 	***************************/
-	static T EaseOutCubic(float time, const T* start, const T* goal)
+	template <class T>
+	static T OutCubic(float time, const T& start, const T& end)
 	{
-		if (time > 1.0f)
-			time = 1.0f;
+		if (time >= 1.0f)
+			return end;
 
-		if (time < 0.0f)
-			time = 0.0f;
+		if (time <= 0.0f)
+			return start;
 
-		return -(*goal - *start) * time * (time - 2.0f) + *start;
+		return -(end - start) * time * (time - 2.0f) + start;
 	}
 
 	/***************************
-	InOutCubic計算処理
+	InOutCubic計算クラス
 	***************************/
-	static T EaseInOutCubic(float time, const T* start, const T* goal)
+	template <class T>
+	static T InOutCubic(float time, const T& start, const T& end)
 	{
-		if (time > 1.0f)
-			time = 1.0f;
+		if (time >= 1.0f)
+			return end;
 
-		if (time < 0.0f)
-			time = 0.0f;
+		if (time <= 0.0f)
+			return start;
 
 		time *= 2.0f;
 
-		if (time < 1)
-		{
-			return (*goal - *start) / 2.0f * time * time + *start;
-		}
+		if (time < 1.0f)
+			return (end - start) / 2.0f * time * time + start;
 		else
 		{
 			time -= 1.0f;
-			return -(*goal - *start) / 2.0f * (time * (time - 2) - 1) + *start;
+			return -(end - start) / 2.0f * (time * (time - 2.0f) - 1.0f) + start;
 		}
 	}
 
 	/***************************
-	Linear計算処理
+	Linear計算クラス
 	***************************/
-	static T EaseLinear(float time, const T* start, const T* goal)
+	template <class T>
+	static T Linear(float time, const T& start, const T& end)
 	{
-		if (time > 1.0f)
-			time = 1.0f;
+		if (time >= 1.0f)
+			return end;
 
-		if (time < 0.0f)
-			time = 0.0f;
+		if (time <= 0.0f)
+			return start;
 
-		return (*goal - *start) * time + *start;
+		return (end - start) * time + start;
 	}
 
 	/***************************
-	InExponential計算処理
+	InExponential計算クラス
 	***************************/
-	static T EaseInExponential(float time, const T* start, const T* goal)
+	template<class T>
+	static T InExponential(float time, const T& start, const T& end)
 	{
-		if (time > 1.0f)
-			time = 1.0f;
+		if (time >= 1.0f)
+			return end;
 
-		if (time < 0.0f)
-			time = 0.0f;
+		if (time <= 0.0f)
+			return start;
 
-		return (*goal - *start) *  powf(2, 10 * (time - 1)) + *start;
+		return (end - start) * powf(2.0f, 10.0f * (time - 1.0f)) + start;
 	}
 
 	/***************************
-	OutExponential計算処理
+	OutExponential計算クラス
 	***************************/
-	static T EaseOutExponential(float time, const T* start, const T* goal)
+	template<class T>
+	static T OutExponential(float time, const T& start, const T& end)
 	{
-		if (time > 1.0f)
-			time = 1.0f;
+		if (time >= 1.0f)
+			return end;
 
-		if (time < 0.0f)
+		if (time <= 0.0f)
+			return start;
 
-			time = 0.0f;
-		return (*goal - *start) * (-powf(2, (-10 * time)) + 1) + *start;
+		return (end - start) * (-powf(2.0f, (-10 * time)) + 1.0f) + start;
 	}
 
 	/***************************
 	InOutExponential計算処理
 	***************************/
-	static T EaseInOutExponential(float time, const T* start, const T* goal)
+	template<class T>
+	static T InOutExponential(float time, const T& start, const T& end)
 	{
-		if (time > 1.0f)
-			time = 1.0f;
+		if (time >= 1.0f)
+			return end;
 
-		if (time < 0.0f)
-			time = 0.0f;
+		if (time <= 0.0f)
+			return start;
 
-		return *start;
+		float per = Math::Bezier3(D3DXVECTOR2(0.0f, 0.0f), D3DXVECTOR2(1.0f, 0.0f), D3DXVECTOR2(0.0f, 1.0f), D3DXVECTOR2(1.0f, 1.0f), time).y;
+		return (end - start) * per + start;
 	}
-};
 
+};
 #endif
