ca0e6d2b4448cec65df9a4a947f72a2ceb6da50c
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-08-18 11:26:31 +0900

    IState変更

--- Framework/Pattern/BaseState.h
+++ Framework/Pattern/BaseState.h
@@ -1,11 +1,11 @@
 //=============================================================================
 //
-// ステートインターフェースヘッダ [IState.h]
+// ステートインターフェースヘッダ [BaseState.h]
 // Author : 立花雄太
 //
 //=============================================================================
-#ifndef _ISTATE_H_
-#define _ISTATE_H_
+#ifndef _BASESTATE_H_
+#define _BASESTATE_H_
 
 #define STATE_CONTINUOUS	(0)		//ステートを続行する
 #define STATE_FINISHED		(1)		//ステートは終了した
@@ -14,10 +14,10 @@
 IStateMachineクラス
 継承先でOnUpdate(), OnStart()を実装する
 *****************************************************/
-template <class T>
+template <class T, class ENUM>
 class IState {
 public:
-	virtual int OnUpdate(T& entity) = 0;
+	virtual ENUM OnUpdate(T& entity) = 0;
 	virtual void OnStart(T& entity) = 0;
 };
 
