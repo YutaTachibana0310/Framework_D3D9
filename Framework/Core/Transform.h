e1119f7675572fbb782fdc2cbc891d67d76dd3fe
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-08-22 12:06:26 +0900

    BillboardObjectとBillboardTransformを作成

--- Framework/Core/Transform.h
+++ Framework/Core/Transform.h
@@ -24,45 +24,39 @@ public:
 	Transform();
 	Transform(D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR3 scale);
 	Transform(const Transform&);
+	virtual ~Transform() {}
 
 	//演算子
-	Transform operator = (const Transform&);
+	virtual Transform operator = (const Transform&);
 
 	//移動処理
-	void Move(float x, float y, float z);
-	void Move(const D3DXVECTOR3& velocity);
-	void SetPosition(const D3DXVECTOR3& position);
-	D3DXVECTOR3 GetPosition();
+	virtual void Move(const D3DXVECTOR3& velocity);
+	virtual void SetPosition(const D3DXVECTOR3& position);
+	virtual D3DXVECTOR3 GetPosition();
 
 	//回転処理
-	void Rotate(float degX, float degY, float degZ);
-	void Rotate(float deg, const D3DXVECTOR3& axis);
-	void SetRotation(float x, float y, float z);
-	void SetRotation(const D3DXVECTOR3& rotation);
-	void SetRotation(const D3DXQUATERNION& rotation);
-	D3DXVECTOR3 GetEulerAngle();
-	D3DXQUATERNION GetRotation();
+	virtual void Rotate(float degreeX, float degreeY, float degreeZ);
+	virtual void Rotate(float degree, const D3DXVECTOR3& axis);
+	virtual void SetRotation(const D3DXVECTOR3& rotation);
+	virtual void SetRotation(const D3DXQUATERNION& rotation);
+	virtual D3DXVECTOR3 GetEulerAngle();
+	virtual D3DXQUATERNION GetRotation();
 
 	//スケール処理
-	void Scale(float deltaX, float deltaY, float deltaZ);
-	void ScaleX(float delta);
-	void ScaleY(float delta);
-	void ScaleZ(float delta);
-	void SetScale(const D3DXVECTOR3& scale);
-	D3DXVECTOR3 GetScale();
+	virtual void Scale(const D3DXVECTOR3& delta);
+	virtual void SetScale(const D3DXVECTOR3& scale);
+	virtual D3DXVECTOR3 GetScale();
 
 	//向き
-	D3DXVECTOR3 Forward();
-	D3DXVECTOR3 Right();
-	D3DXVECTOR3 Up();
+	virtual D3DXVECTOR3 Forward();
+	virtual D3DXVECTOR3 Right();
+	virtual D3DXVECTOR3 Up();
 
 	//ワールド変換設定処理
-	void SetWorld(const D3DXMATRIX* parent = NULL);
-	void SetWorldInvView(const D3DXMATRIX* parent = NULL);
-	D3DXMATRIX GetMatrix();
-	D3DXMATRIX GetBillboardMtx();
+	virtual void SetWorld(const D3DXMATRIX* parent = NULL);
+	virtual D3DXMATRIX GetMatrix();
 	
-private:
+protected:
 	D3DXVECTOR3 position;	//座標
 	D3DXVECTOR3 scale;	//スケール
 	D3DXQUATERNION rotation;	//回転
